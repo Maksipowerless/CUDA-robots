04a004d70ddcf591f0a3a5c7cbec0984045c521a
tm5 <tm3u5@login1.cluster>
2017-06-12 12:24:07 +0300

    first commit

---
+++ program.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <time.h>
+#include <cmath>
+#include <vector>
+#include<stdlib.h>
+
+using namespace std;
+#define N 100
+
+int map[N][N];
+int heightOfRobot = 50;
+int heightOfStantion = 120;
+int xStatic = 66;
+int yStatic = 55;
+
+
+void findDarkArea(vector<pair<int, int> >& coord, vector<pair<int, int> >& versities)
+{
+    float alpha = 0;
+    pair<int,int> tempPair;
+    pair<int,int> currentPoint;
+
+    while(versities.size() != 0)
+    {
+        //поиск точки с меньшим полярным углом от стационарной
+        for(int i=0; i<versities.size(); i++)
+        {
+            float A = versities[i].first;
+            float B = sqrt(pow(versities[i].first, 2) + pow((heightOfStantion - versities[i].second), 2));
+            float temp = 0;
+            temp = A/B;
+            if(temp > alpha)
+            {
+                alpha = temp;
+                tempPair = coord[i];
+                currentPoint = versities[i];
+            }
+        }
+
+        //проверка на видимость всех точек за текущей
+	int number  = 0;
+	for(int j=0; j<versities.size(); j++)
+	{
+	  if(versities[j] == currentPoint)
+		{
+		  number = j;
+		  break;
+		}
+	}	
+
+        for(int i = number + 1; i< versities.size(); i++)
+        {
+            if(versities[i].second + heightOfRobot < currentPoint.second)
+            {
+                map[coord[i].first][coord[i].second] = 999;
+                coord.erase(coord.begin() + i);
+                versities.erase(versities.begin() + i);
+            }
+        }
+
+	for(int j=0; j<coord.size(); j++)
+	{
+		if(coord[j] == tempPair)
+		{
+		  coord.erase(coord.begin() + j);
+		  versities.erase(versities.begin() +j);
+		  break;
+		}
+	}
+    
+        alpha = 0;
+    }
+}
+
+
+void bresenhamLine(int x1, int y1, int x2, int y2)
+{
+    const int deltaX = abs(x2 - x1);
+    const int deltaY = abs(y2 - y1);
+    const int signX = x1 < x2 ? 1 : -1;
+    const int signY = y1 < y2 ? 1 : -1;
+    int error = deltaX - deltaY;
+    int count = 0;//cчеткик кол-ва точек
+
+    vector<pair<int,int> > versities;
+    vector<pair<int,int> > coord;
+
+    while(x1 != x2 || y1 != y2)
+    {
+        count++;
+        const int error2 = error * 2;
+        if(error2 > -deltaY)
+        {
+            error -= deltaY;
+            x1 += signX;
+        }
+        if(error2 < deltaX)
+
+        {
+            error += deltaX;
+            y1 += signY;
+        }
+
+        pair<int,int> pair;
+        pair.first = count;
+        pair.second = map[x1][y1];
+        versities.push_back(pair);
+
+        pair.first = x1;
+        pair.second = y1;
+        coord.push_back(pair);
+    }
+	findDarkArea(coord, versities);
+}
+
+int main()
+{
+    srand(time(0));
+       for(int i=0; i<N; i++)
+        for(int j=0; j<N; j++)
+            map[i][j] = rand() % 80 +5;
+   
+    map[xStatic][yStatic] = heightOfStantion;
+
+    for(int i=0; i<N; i++)
+	{
+	  bresenhamLine(xStatic, yStatic, i,0);
+	  bresenhamLine(xStatic, yStatic, i, N-1);
+          bresenhamLine(xStatic, yStatic, 0, i);
+          bresenhamLine(xStatic, yStatic, N-1, i);
+	}
+
+	for(int i=0; i< N; i++)
+	{
+		for(int j=0; j< N; j++)
+		{
+			cout<<map[i][j] <<" ";
+		}
+		cout<<"\n";
+	}
+    return 0;
+}
